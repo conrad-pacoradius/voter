db3ce22e6490d2e196053d2bbbf0aed3f9239ef7
Conrad Bartels Daal <conrad@pacoradius.com>
2020-11-27 19:59:38 +0100

    added tests

---
+++ src/voter.domain/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace voter.domain
+{
+    public class Class1
+    {
+    }
+
+    public class Voter
+    {
+        public string Name { get; set; }
+    }
+
+    public class Election
+    {
+    
+    }
+    public class Candidate{
+
+    }
+    public class VoterRegistration{
+        
+    }
+
+    public class Vote
+    {
+
+    }
+
+}
