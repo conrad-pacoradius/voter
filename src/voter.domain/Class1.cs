cb6e56d8834ea1961d1ae7344b8c2bbef1faf8f4
Conrad Bartels Daal <conrad@pacoradius.com>
2020-11-27 19:59:44 +0100

    u

--- src/voter.domain/Class1.cs
+++ src/voter.domain/Class1.cs
@@ -1,9 +1,101 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace voter.domain
 {
     public class Class1
     {
+    }
+
+    public class Government
+    {
+
+    }
+
+    /// <summary>
+    /// A polity is an identifiable political entity—any group of people who have a collective identity,
+    /// who are organized by some form of institutionalized social relations, and have a capacity to
+    /// mobilize resources. It is useful then to think of a polity as a political community.
+    /// </summary>
+    public class Polity
+    {
+        public string Name { get; set; }
+
+        public Polity(string name, Constitution constitution)
+        {
+            Name = name;
+        }
+    }
+
+    public class Constitution
+    {
+        public string Preamble { get; set; }
+        public List<ConstitutionArticle> Articles { get; set; }
+        public List<ConstitutionAmendment> Amendments { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime FirstAmendentAt { get; set; }
+        public DateTime LastAmendentAt { get; set; }
+    }
+
+    public class ConstitutionAmendment
+    {
+        public string Name { get; set; }
+        public List<ConstitutionSection> Sections { get; set; }
+    }
+
+    public class ConstitutionArticle
+    {
+        public string Name { get; set; }
+        public List<ConstitutionSection> Sections { get; set; }
+    }
+
+    public class ConstitutionSection
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Legislature
+    {
+
+    }
+
+    /// <summary>
+    /// A sovereign state in international law is a political entity that is represented by one centralized government that has sovereignty over a geographic area.
+    /// </summary>
+    public class Country
+    {
+        public string Name { get; set; }
+
+        public Country(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set;}
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person(string name, string lastName, DateTime dateOfBirth )
+        {
+            Name = name;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+    }
+
+    public class Citizen
+    {
+
+    }
+
+    public class Constituent
+    {
+
     }
 
     public class Voter
@@ -13,16 +105,83 @@ namespace voter.domain
 
     public class Election
     {
-    
+        public DateTime VotingStartDate { get; set; }
+        public DateTime VotingEndDate { get; set; }
+
+        public DateTime VoterRegistrationStartDate { get; set; }
+        public DateTime VoterRegistrationEndDate { get; set; }
+
+        public List<Voter> Voters { get; }
+        public List<Ballot> Votes { get; }
+        public List<Candidate> Candidates { get; set; }
+
+
+        public Election()
+        {
+
+        }
+
+        public void RegisterVoter(Voter voter)
+        {
+
+        }
     }
-    public class Candidate{
+
+    public class ElectionPeriod
+    {
 
     }
-    public class VoterRegistration{
-        
+
+    public class Referendum
+    {
+
     }
 
-    public class Vote
+    public class Initiative
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+
+    public enum ElectionType
+    {
+        Plurality,
+        Majority,
+        Proportional
+    }
+
+    public class Ballot
+    {
+
+    }
+
+    public class VoterRegistration
+    {
+
+    }
+
+    public class EleectionVoterRegistrationRules
+    {
+
+    }
+
+    public class PoliticalParty
+    {
+
+    }
+
+    public class Candidate
+    {
+
+    }
+
+    public class ElectionCandidateRegistrationRules
+    {
+
+    }
+
+
+    public class CandidateRegistration
     {
 
     }
--- test/Voter.Domain.UnitTests/UnitTest1.cs
+++ test/Voter.Domain.UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using voter.domain;
 using Xunit;
 
 namespace Voter.Domain.UnitTests
@@ -8,7 +9,45 @@ namespace Voter.Domain.UnitTests
         [Fact]
         public void Test1()
         {
+            //Add Population
+            var person1 = new Person("Person 1", "Person 1 Last Name", new DateTime(1980, 1, 1));
+            var person2 = new Person("Person 2", "Person 2 Last Name", new DateTime(1980, 1, 2));
+            var person3 = new Person("Person 3", "Person 3 Last Name", new DateTime(1980, 1, 3));
+            var person4 = new Person("Person 4", "Person 4 Last Name", new DateTime(1980, 1, 4));
+            var person5 = new Person("Person 5", "Person 5 Last Name", new DateTime(1980, 1, 5));
+            var person6 = new Person("Person 6", "Person 6 Last Name", new DateTime(1980, 1, 6));
+            var person7 = new Person("Person 7", "Person 7 Last Name", new DateTime(1980, 1, 7));
+            var person8 = new Person("Person 8", "Person 8 Last Name", new DateTime(1980, 1, 8));
+            var person9 = new Person("Person 9", "Person 9 Last Name", new DateTime(1980, 1, 9));
+            var person10 = new Person("Person 10", "Person 10 Last Name", new DateTime(1980, 1, 10));
+            var person11 = new Person("Person 11", "Person 11 Last Name", new DateTime(1980, 1, 11));
+            var person12 = new Person("Person 12", "Person 12 Last Name", new DateTime(1980, 1, 12));
+            var person13 = new Person("Person 13", "Person 13 Last Name", new DateTime(1980, 1, 13));
+            var person14 = new Person("Person 14", "Person 14 Last Name", new DateTime(1980, 1, 14));
+            var person15 = new Person("Person 15", "Person 15 Last Name", new DateTime(1980, 1, 15));
+            var person16 = new Person("Person 16", "Person 16 Last Name", new DateTime(1980, 1, 16));
+            var person17 = new Person("Person 17", "Person 17 Last Name", new DateTime(1980, 1, 17));
+            var person18 = new Person("Person 18", "Person 18 Last Name", new DateTime(1980, 1, 18));
+            var person19 = new Person("Person 19", "Person 19 Last Name", new DateTime(1980, 1, 19));
+            var person20 = new Person("Person 20", "Person 20 Last Name", new DateTime(1980, 1, 20));
 
+            //Create Country
+            var country1 = new Country("Country 1");
+
+
+            //Create Political Entity
+            var polity1 = new Polity("Political Entity 1");
+
+            // Create Election
+            var election = new Election();
+
+            //Register Voters
+            
+
+
+
+            //Create new initiative
+            var initiative1 = new Initiative();
         }
     }
 }
